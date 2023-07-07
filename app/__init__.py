3d6155c4a0496442cd01d5209d5329b58952cfae
JSamgw <grantjessie01@gmail.com>
2023-07-06 22:41:34 -0400

    Finalized homepage views with data and filters

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from app.routes import home, dashboard
+from app.db import init_db
+from app.utils import filters
 
 
 def create_app(test_config=None):
   # set up app config
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
@@ -17,5 +22,6 @@ def create_app(test_config=None):
   # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+  init_db(app)
 
   return app
