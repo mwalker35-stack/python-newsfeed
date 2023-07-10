24f7f0c1f4259fe9a4c67d0f89208cd5efd253d5
JSamgw <grantjessie01@gmail.com>
2023-07-10 12:19:11 -0400

    Updated and deleted data by using SQLAlchemy methods, Created a custom decorator function to act as route middleware

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,6 @@
 import sys
+from app.utils.auth import login_required
+from app.models import User, Post, Comment, Vote
 from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
@@ -66,4 +68,110 @@ def signup():
   return jsonify(id = newUser.id)
 
 
+@bp.route('/comments', methods=['POST'])
+@login_required
+def comment():
+  data = request.get_json()
+  db = get_db()
 
+  try:
+    # create a new comment
+    newComment = Comment(
+      comment_text = data['comment_text'],
+      post_id = data['post_id'],
+      user_id = session.get('user_id')
+  )
+
+    db.add(newComment)
+    db.commit()
+
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Comment failed'), 500.
+  
+  return jsonify(id = newComment.id)
+
+@bp.route('/posts/upvote', methods=['PUT'])
+@login_required
+def upvote():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new vote with incoming id and session id
+    newVote = Vote(
+      post_id = data['post_id'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newVote)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Upvote failed'), 500
+
+  return '', 204
+
+@bp.route('/posts', methods=['POST'])
+@login_required
+def create():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new post
+    newPost = Post(
+      title = data['title'],
+      post_url = data['post_url'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newPost)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Post failed'), 500
+
+  return jsonify(id = newPost.id)
+
+@bp.route('/posts/<id>', methods=['PUT'])
+@login_required
+def update(id):
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # retrieve post and update title property
+    post = db.query(Post).filter(Post.id == id).one()
+    post.title = data['title']
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Post not found'), 404
+
+  return '', 204
+
+@bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
+def delete(id):
+  db = get_db()
+
+  try:
+    # delete post from db
+    db.delete(db.query(Post).filter(Post.id == id).one())
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Post not found'), 404
+
+  return '', 204
